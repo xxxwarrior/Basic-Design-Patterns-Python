96d4eded43db7f798bd7bafbcd5551c439a856aa
xxxwarrior <sklmasha@yandex.ru>
2020-11-06 22:27:53 +0300

    Add Chain of Responsibility pattern

---
+++ Chain-of-Responsibility/README.md
@@ -0,0 +1,3 @@
+Chain of Responsibility is a behavioral design pattern which is used when you want to give more than one object a chance to handle a request.
+
+![alt text](https://github.com/xxxwarrior/Basic-Design-Patterns-Python/blob/main/Chain-of-Responsibility/diagram.jpg?raw=true)
---
+++ Chain-of-Responsibility/chain-of-responsibility.py
@@ -0,0 +1,45 @@
+"""
+Chain of Responsibility allows you to give request handling to a chain of objects until one of them handles it.
+"""
+
+class Handler:
+    def __init__(self, nextHandler=None):
+        self.nextHandler = nextHandler
+        self.requestToHandle = ""
+
+    def handleRequest(self, request):
+        if request != self.requestToHandle and self.nextHandler:
+            self.nextHandler.handleRequest(request)
+        elif request != self.requestToHandle and not self.nextHandler:
+            print(f'Unsupported requeset')
+        else: print(f'Request is handled by {self.__class__.__name__}')
+         
+
+class ConcreteHandler(Handler):
+    def __init__(self, nextHandler=None):
+        self.nextHandler = nextHandler
+        self.requestToHandle = "Concrete Request"
+
+        
+class AnotherConcreteHandler(Handler):
+    def __init__(self, nextHandler=None):
+        self.nextHandler = nextHandler
+        self.requestToHandle = "Another Request"
+
+
+class OneMoreHandler(Handler):
+    def __init__(self, nextHandler=None):
+        self.nextHandler = nextHandler
+        self.requestToHandle = "One More Request"
+
+
+
+
+if __name__ == "__main__":
+    handler = ConcreteHandler()
+    handler1 = AnotherConcreteHandler()
+    handler2 = OneMoreHandler()
+    handler.nextHandler = handler1
+    handler1.nextHandler = handler2
+
+    handler.handleRequest("One More Request")
