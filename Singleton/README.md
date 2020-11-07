6d1f60b864f798cbba4cc7fa0ade3d3f21ee0665
xxxwarrior <sklmasha@yandex.ru>
2020-11-07 21:55:56 +0300

    Add Singleton pattern

---
+++ Singleton/README.md
@@ -0,0 +1,4 @@
+Singleton is a creational design pattern that ensures a class has only one instance, and provides a global point of access to it.
+
+
+![alt text](https://github.com/xxxwarrior/Basic-Design-Patterns-Python/blob/main/Singleton/diagram.jpg?raw=true)
---
+++ Singleton/singleton.py
@@ -0,0 +1,30 @@
+"""
+Singleton ensures that there's only one instance of the class and provides only one global point of access to it.
+"""
+
+
+class Singleton:
+    uniqueInstance = None
+
+    def __init__(self) -> None:
+        if Singleton.uniqueInstance != None:
+            raise Exception("Can't create another instance!")
+        else: Singleton.uniqueInstance = self
+
+    @staticmethod
+    def getInstance() -> Singleton:
+        if Singleton.uniqueInstance == None:
+            Singleton()
+        return Singleton.uniqueInstance
+
+        
+
+        
+if __name__ == "__main__":
+    singleton1 = Singleton()
+    print(Singleton.getInstance())
+
+    try: 
+        Singleton()
+    except: 
+        print("It doesn't work exactly as expected!")
